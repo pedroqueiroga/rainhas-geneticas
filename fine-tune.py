d2b6c7823f013d05183d96b73ab5c71f380c56f4
Pedro de Souza Queiroga <psq@ug4c31.windows.cin.ufpe.br>
2019-03-27 20:22:54 -0300

    fine-tune.py

---
+++ fine-tune.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+''' 
+This program's intention is to search for the best parameters in rainhas.py, through
+the use of genetic algorithms.
+'''
+
+from rainhas import RainhasGeneticas
+
+rainhas = RainhasGeneticas(pop_init_size=10, mutation_chance=0.3, grow_population=False, population_limit=20,
+                            die_by_age=False, converge_all=True, verbose=False)
+rainhas.solve_it()
+print(rainhas.evaluate(rainhas.population))
