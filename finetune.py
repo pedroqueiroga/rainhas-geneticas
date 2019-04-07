4848ee0d88377cf947329fa703f252f04777d174
Pedro Queiroga <psq@cin.ufpe.br>
2019-04-07 14:21:54 -0300

    progresso melhorado

--- finetune.py
+++ finetune.py
@@ -13,15 +13,16 @@ mutation_chance=0.1
 grow_population=False
 population_limit=20
 die_by_age=False
-converge_all=True
+converge_all=False
 verbose=False
 
 rainhas = RainhasGeneticas(pop_init_size, mutation_chance, grow_population, population_limit, die_by_age, converge_all, verbose)
 
 runs = []
 t_0 = time.time()
-k=100
+k=1000
 print("array of integers representation.")
+print("population size =", pop_init_size)
 if grow_population:
     print("population growth activated. limit =", population_limit,end='. ')
 if die_by_age:
@@ -32,13 +33,13 @@ if grow_population or die_by_age or converge_all:
     print()
 print("-"*80)
 print("number of runs:",k)
-print("starting... ",end='', flush=True)
+print("starting... ")
 for i in range(k):
     runs.append(rainhas.solve_it())
-    print("", i+1,"",end='', flush=True)
+    print("\r", i+1, " of ", k, " (%.2f" % ((i+1)/k * 100), " %)", sep='', end='', flush=True)
     # print(rainhas.evaluate(rainhas.population))
 t_f = time.time()
-print("done.")
+print(" done.")
 print("ran for %.2f seconds." % (t_f-t_0))
 print("successes:", sum(int(i["success"]) for i in runs))
 print("average number of generations:", sum(i["numberOfGenerations"] for i in runs)/len(runs))
@@ -47,10 +48,3 @@ print("largest number of generations needed:", max(i["numberOfGenerations"] for 
 print("average individual evaluation:", sum(i["averageIndividual"] for i in runs)/len(runs))
 print("average worst individual:", sum(i["worstIndividual"] for i in runs)/len(runs))
 
-"""
-print("-"*80)
-for r in runs:
-    solution_index = r["evaluation"].index(0)
-    rainhas.visualize_gene(r["population"][solution_index])
-    print("-"*80)
-"""
